176d6a07466b6ec7aaff7e0d0e50317c503b0284
Agung Tata Fahreza <fahreza.atf@gmail.com>
2021-07-21 18:22:49 +0700

    m

--- index7.php
+++ index7.php
@@ -1,20 +1,34 @@
-<?php
-$array = [
-    '1' => [
-        ['destination' => 'Darussalam', 'bus' => 'tr-01', 'eta' => '12', 'aval' => '12'],
-        ['destination' => 'Darussalam', 'bus' => 'tr-01', 'eta' => '12', 'aval' => '12'],
-        ['destination' => 'Darussalam', 'bus' => 'tr-01', 'eta' => '12', 'aval' => '12'],
-    ],
-    '2' => [
-        ['destination' => 'Darussalam', 'bus' => 'tr-01', 'eta' => '12', 'aval' => '12'],
-        ['destination' => 'Darussalam', 'bus' => 'tr-01', 'eta' => '12', 'aval' => '12'],
-        ['destination' => 'Darussalam', 'bus' => 'tr-01', 'eta' => '12', 'aval' => '12'],
-    ],
-    '3' => [
-        ['destination' => 'Darussalam', 'bus' => 'tr-01', 'eta' => '12', 'aval' => '12'],
-        ['destination' => 'Darussalam', 'bus' => 'tr-01', 'eta' => '12', 'aval' => '12'],
-        ['destination' => 'Darussalam', 'bus' => 'tr-01', 'eta' => '12', 'aval' => '12'],
-    ],
-];
+<!DOCTYPE html>
+<html lang="en">
 
-print_r($array);
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <input type="text" class="angka"><br>
+        <input type="text" class="angka"><br>
+        <input type="text" class="angka"><br>
+        <button type="button" id="go">Go</button>
+    </form>
+    <script src="jquery.js">
+    </script>
+    <script>
+        $(document).ready(function() {
+            // console.log("!");
+            // alert('hai');
+            $("#go").click(function() {
+                var inputs = $(".angka");
+                for (var i = 0; i < inputs.length; i++) {
+                    console.log($(inputs[i]).val());
+                }
+            });
+        });
+    </script>
+</body>
+
+</html>
