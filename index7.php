39e272b6294fd2420897acf36279d00485bf5385
Agung Tata Fahreza <fahreza.atf@gmail.com>
2021-12-26 10:10:04 +0700

    m

--- index7.php
+++ index7.php
@@ -1,20 +1,15 @@
 <?php
 
-$a = false;
-$b = false;
+$timeStamp = strtotime('18-12-2020 10:11:12');
 
-if ($a !== null) {
-    echo "not null";
+if (date('Y-m-d') == date('Y-m-d', $timeStamp)) {
+    echo date('H:i', $timeStamp);
+} elseif (date('d-m-Y', $timeStamp) == date('d-m-Y', strtotime('yesterday'))) {
+    echo 'Yesterday';
+} elseif ($timeStamp > strtotime('-1 week')) {
+    echo date('D', $timeStamp);
+} elseif (date('Y', $timeStamp) == date('Y')) {
+    echo date('d M', $timeStamp);
 } else {
-    echo "null";
+    echo date('d.m.Y', $timeStamp);
 }
-
-// if ($a) {
-//     echo "true";
-// } else {
-//     if ($a === false) {
-//         echo "false";
-//     } else {
-//         echo "null";
-//     }
-// }
